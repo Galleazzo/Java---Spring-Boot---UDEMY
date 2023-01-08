10f72ef857eb86111c6e57aa98860ded25ee6c5a
Galleazzo <paulogalleazzo1@gmail.com>
2023-01-08 19:33:48 -0300

    calculadora

---
+++ src/main/java/br/com/teste/exerciciosSB/controlers/CalculadoraControler.java
@@ -0,0 +1,25 @@
+package br.com.teste.exerciciosSB.controlers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/calculadora")
+public class CalculadoraControler {
+
+	
+	@GetMapping("/somar/{n1}/{n2}")
+	public int somar(@PathVariable int n1, @PathVariable int n2) {
+		return n1 + n2;
+	}
+	
+	@GetMapping(path = "subtrair")
+	public int subtrair(@RequestParam(name = "n1") int n1,@RequestParam(name = "n2") int n2) {
+		return n1 + n2;
+	}
+	
+	
+}
--- src/main/java/br/com/teste/exerciciosSB/controlers/ClienteControler.java
+++ src/main/java/br/com/teste/exerciciosSB/controlers/ClienteControler.java
@@ -1,7 +1,9 @@
 package br.com.teste.exerciciosSB.controlers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.teste.exerciciosSB.models.Cliente;
@@ -14,6 +16,16 @@ public class ClienteControler {
 	public Cliente obterCliente() {
 		return new Cliente(28, "Paulo", "123.456.789-00");
 				
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "980.890.342.00");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "João augusto", "111.222.333-44");
 	}
 	
 }
