7178048ba3e9ca48634b51ddf772e103990cf6f2
Galleazzo <paulogalleazzo1@gmail.com>
2023-01-09 16:19:40 -0300

    metodos crud

--- src/main/java/br/com/teste/exerciciosSB/controlers/ProdutoControler.java
+++ src/main/java/br/com/teste/exerciciosSB/controlers/ProdutoControler.java
@@ -1,14 +1,24 @@
 package br.com.teste.exerciciosSB.controlers;
 
+import java.lang.StackWalker.Option;
+import java.lang.reflect.Method;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.teste.exerciciosSB.model.entities.Produto;
 import br.com.teste.exerciciosSB.model.repositories.ProdutoRepositorio;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/api/produtos")
@@ -17,13 +27,33 @@ public class ProdutoControler {
 	@Autowired
 	private ProdutoRepositorio produtoRepositorio;
 
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	//@PostMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		
+		produtoRepositorio.save(produto);
 		return produto;
 	}
 	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepositorio.findAll();
+	}
 	
 	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable Integer id) {
+		return produtoRepositorio.findById(id);
+	}
+	
+	/*
+	 * @PutMapping public Produto alterarProduto(@Valid Produto produto) {
+	 * produtoRepositorio.save(produto); return produto; }
+	 */
+	
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirPorduto(@PathVariable Integer id) {
+		produtoRepositorio.deleteById(id);
+	}
 }
--- src/main/java/br/com/teste/exerciciosSB/model/entities/Produto.java
+++ src/main/java/br/com/teste/exerciciosSB/model/entities/Produto.java
@@ -6,6 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
@@ -14,15 +17,27 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank
 	private String nome;
+	
+	@Min(0)
+	private Double precoNormal;
+	
+	@Min(0)
+	@Max(1)
+	private Double desconto;
 	
 	public Produto() {
 		
 	}
+
 	
-	public Produto(String nome) {
+
+	public Produto(String nome, Double precoNormal, Double precoComDesconto) {
 		super();
 		this.nome = nome;
+		this.precoNormal = precoNormal;
+		this.desconto = precoComDesconto;
 	}
 
 
@@ -41,6 +56,22 @@ public class Produto {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Double getPrecoNormal() {
+		return precoNormal;
+	}
+
+	public void setPrecoNormal(Double precoNormal) {
+		this.precoNormal = precoNormal;
+	}
+
+	public Double getPrecoComDesconto() {
+		return desconto;
+	}
+
+	public void setPrecoComDesconto(Double precoComDesconto) {
+		this.desconto = precoComDesconto;
 	}
 
 	
